09067bc9ee4a7e0997d0f2ad5e47872e5cd962e6
JuricaRT <runtas.j@gmail.com>
2020-12-28 09:02:44 +0100

    update

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -1,63 +1,104 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-void bfs(vector<vector<int>> adj, int start){
-  // dist -> shortest paths
-  // p -> parents
-  //vector<int> dist(adj.size());
-  //vector<int> p(adj.size());
-  vector<bool> visited(adj.size(), false);
+void vanilla_bfs(vector<int> adj_list[], int s, int v){
+  bool visited[v+1];
   queue<int> q;
-  q.push(start);
-  visited[start] = true;
+  visited[s] = true;
+  q.push(s);
   while (!q.empty()){
-    int vertex = q.front();
+    int current_vertex = q.front();
     q.pop();
-    cout << vertex << " ";
-    for (int i = 0; i < adj.size(); i++){
-      if (!visited[i] && adj[vertex][i]){
-        visited[i] = true;
-        q.push(i);
-        //dist[i] = dist[vertex]+1;
-        //p[i] = vertex;
-      }
+    cout << current_vertex << " ";
+    for (auto i : adj_list[current_vertex]){
+      if (visited[i]) continue;
+      visited[i] = true;
+      q.push(i);
     }
   }
 }
 
-int main(){
-  int v, e;
-  cin >> v >> e;
-
-  // redovi predstavljaju koji node je povezan
-  // stupci predstavlja s cim je neki node povezan
-  vector<vector<int>> adj_mat;
-  
-  // v * v matrica popunjena s nulama
-  for (int i = 0; i < v; ++i){
-    vector<int> row(v, 0);
-    adj_mat.push_back(row);
+void shortest_path_bfs(vector<int> adj_list[], int s, int d, int v){
+  int distance[v+1];
+  bool visited[v+1];
+  queue<int> q;
+  distance[s] = 0;
+  visited[s] = true;
+  q.push(s);
+  while (!q.empty()){
+    int current_vertex = q.front();
+    q.pop();
+    for (auto i : adj_list[current_vertex]){
+      if (visited[i] == true) continue;
+      visited[i] = true;
+      distance[i] = distance[current_vertex]+1;
+      q.push(i);
+    }
   }
-   
-  // primjer za v = 5
-  adj_mat[0][1] = 1;
-  adj_mat[0][4] = 1;
-  adj_mat[1][0] = 1;
-  adj_mat[1][2] = 1;
-  adj_mat[1][3] = 1;
-  adj_mat[1][4] = 1;
-  adj_mat[2][1] = 1;
-  adj_mat[2][3] = 1;
-  adj_mat[3][1] = 1;
-  adj_mat[3][2] = 1;
-  adj_mat[3][4] = 1;
-  adj_mat[4][0] = 1;
-  adj_mat[4][1] = 1;
-  adj_mat[4][3] = 1;
- 
-  bfs(adj_mat, 0);
+  cout << distance[d];
+}
 
+void restore_shortest_path_bfs(vector<int> adj_list[], int s, int d, int v){
+  int distance[v+1];
+  bool visited[v+1];
+  int parent[v+1];
+  queue<int> q;
+  distance[s] = 0;
+  visited[s] = true;
+  parent[s] = -1;
+  q.push(s);
+  while (!q.empty()){
+    int current_vertex = q.front();
+    q.pop();
+    for (int i : adj_list[current_vertex]){
+      if (visited[i]) continue;
+      visited[i] = true;
+      distance[i] = distance[current_vertex]+1;
+      parent[i] = current_vertex;
+      q.push(i);
+    }
+  }
+
+  vector<int> restored_path;
+  for (int v = d; v != -1; v = parent[v]){
+    restored_path.push_back(v);
+  }
+  reverse(restored_path.begin(), restored_path.end());
+  for (auto u : restored_path){
+    cout << u << " ";
+  }
+}
+
+int main(){
+  // graph from Guide to Competitive Programming - page 85
+  int v = 6;
+  vector<int> adj_list[v+1];
+  adj_list[1].push_back(2);
+  adj_list[1].push_back(4);
+
+  adj_list[2].push_back(1);
+  adj_list[2].push_back(3);
+  adj_list[2].push_back(5);
+
+  adj_list[3].push_back(2);
+  adj_list[3].push_back(6);
+
+  adj_list[4].push_back(1);
+
+  adj_list[5].push_back(2);
+  adj_list[5].push_back(6);
+  
+  adj_list[6].push_back(3);
+  adj_list[6].push_back(5);
+  
+  vanilla_bfs(adj_list, 1, v);
+  cout << "\n";
+  shortest_path_bfs(adj_list, 1, 3, v); 
+  cout << "\n";
+  restore_shortest_path_bfs(adj_list, 1, 3, v);
+  
   return 0;
 }
