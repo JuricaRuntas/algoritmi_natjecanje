89e32386f5dd6efd8f319e5cb6a322a256102e70
JuricaRT <runtas.j@gmail.com>
2021-01-01 16:09:23 +0100

    update

--- dijkstra/dijkstra.cpp
+++ dijkstra/dijkstra.cpp
@@ -1,31 +1,64 @@
 #include <iostream>
-#include <limits>
 #include <vector>
 #include <utility>
+#include <cstring>
 #include <queue>
-#define INF numeric_limits<int>::max()
-typedef <vector<vector<pair<int,int>>> adj_list;
 using namespace std;
 
-void dijkstra(adj_list, adj, int src){
-  vector<int> processed(adj.size());
-  vector<int> dist(adj.size(), INF);
-  priority_queue<pair<int,int>> q;
-  dist[src] = 0;
-  q.push({0, src});
+const int INF = 1e9;
 
+void dijkstra(vector<pair<int, int>> adj_list[], int s, int n){
+  bool visited[n+1];
+  int dist[n+1];
+  priority_queue<pair<int, int>> q;
+  
+  for (int i = 0; i <= n; ++i){
+    visited[i] = false;
+    dist[i] = INF;
+  }
+
+  dist[s] = 0;
+  q.push({0, s});
+  
   while (!q.empty()){
     int a = q.top().second;
     q.pop();
-    if (processed[a]) continue;
-    processsed[a] = true;
-    for (auto u : adj[a]){
+    if (visited[a]) continue;
+    visited[a] = true;
+    for (pair<int, int> u : adj_list[a]){
       int b = u.first, w = u.second;
-      if (dist[a]+w < dist[b]){
-        dist[b] = dist[a]+w;
-        q.push({-dist[b], b});
-      }
+      dist[b] = min(dist[b], dist[a]+w);
+      q.push({-dist[b], b});
     }
   }
+  for (int i = 1; i <= n; ++i){
+    cout << dist[i] << " ";
+  }
+}
 
+int main(){
+  int n = 5;
+  vector<pair<int, int>> adj_list[n+1];
+
+  // example graph from Guide to Competitive Programming - page 90
+  adj_list[1].push_back({2, 5});
+  adj_list[1].push_back({4, 9});
+  adj_list[1].push_back({5, 1});
+
+  adj_list[2].push_back({1, 5});
+  adj_list[2].push_back({3, 2});
+
+  adj_list[3].push_back({2, 2});
+  adj_list[3].push_back({4, 6});
+  
+  adj_list[4].push_back({1, 9});
+  adj_list[4].push_back({3, 6});
+  adj_list[4].push_back({5, 2});
+
+  adj_list[5].push_back({1, 1});
+  adj_list[5].push_back({4, 2});
+  
+  dijkstra(adj_list, 1, n);
+
+  return 0;
 }
