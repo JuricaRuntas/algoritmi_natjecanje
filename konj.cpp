5262d3c73df1f7268bf19b1e1d59a2b0f3d52c45
JuricaRT <runtas.j@gmail.com>
2021-02-08 18:46:46 +0100

    add

--- konj.cpp
+++ konj.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <deque>
+#include <algorithm>
 using namespace std;
 vector<vector<int>> visited;
 
@@ -8,32 +9,24 @@ int solve(vector<vector<int>> ploca, int r, int s, int x, int y){
   vector<int> pomicanja_po_x = {-1, -1, -2, -2, 1, 1, 2, 2};
   vector<int> pomicanja_po_y = {-2, 2, -1, 1, -2, 2, -1, 1};
   int suma = ploca[x][y];
-  queue<pair<int,int>> q;
-  q.push({x, y});
+  deque<pair<int,int>> q;
+  q.push_back({x, y});
   while (!q.empty()){
     pair<int,int> curr = q.front();
-    q.pop();
+    q.pop_front();
+    if (visited[curr.first][curr.second] == 1) continue;
     visited[curr.first][curr.second] = 1;
     for (int i = 0; i < 8; ++i){
       int novi_x = curr.first+pomicanja_po_x[i];
       int novi_y = curr.second+pomicanja_po_y[i];
       if (novi_x < 0 || novi_y < 0 || novi_x >= r || novi_y >= s) continue;
-      if (visited[novi_x][novi_y] != 1){
-        q.push({novi_x, novi_y});
+      if (visited[novi_x][novi_y] != 1 && find(q.begin(), q.end(), make_pair(novi_x, novi_y)) == q.end()){
+        q.push_back({novi_x, novi_y});
         suma += ploca[novi_x][novi_y];
       }
     }
   }
   return suma;
-}
-
-void ispisi_plocu(vector<vector<int>> ploca){
-  for (int i = 0; i < ploca.size(); ++i){
-    for (int j = 0; j < ploca[0].size(); ++j){
-      cout << ploca[i][j] << " ";
-    }
-    cout << "\n";
-  } 
 }
 
 int main(){
@@ -51,7 +44,7 @@ int main(){
   int max_rezultat = 0;
   for (int i = 0; i < r; ++i){
     for (int j = 0; j < s; ++j){
-      if (ploca[i][j] != 0){
+      if (visited[i][j] != 1){
         max_rezultat = max(max_rezultat, solve(ploca, r, s, i, j));
       }
     }
